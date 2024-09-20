555a520149982c0e96a08fa5c3445de3d7bdd474
Saumya Singh <singhsaumyait034@gmail.com>
2024-09-20 22:08:44 +0530

    Base Page

--- src/main/java/myPages/BasePage.java
+++ src/main/java/myPages/BasePage.java
@@ -1,2 +1,32 @@
-package myPages;public class BasePage {
+package myPages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class BasePage extends Page{
+
+    @Override
+    public String getPageTitle() {
+        return null;
+    }
+
+    @Override
+    public String getPageHeader(By locator) {
+        return null;
+    }
+
+    @Override
+    public WebElement getElement(By locator) {
+        return null;
+    }
+
+    @Override
+    public void waitForElementToBeVisible(By locator) {
+
+    }
+
+    @Override
+    public void waitForPageTitle(String title) {
+
+    }
 }
